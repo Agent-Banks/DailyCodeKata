0bdb0f7dcbf50bb4199239680f1861e51e5997fa
Cody Banks <codydouglasbanks@gmail.com>
2021-02-07 12:10:19 -0500

    count the monkeys code kata

--- Program.cs
+++ Program.cs
@@ -2,11 +2,29 @@
 
 namespace DailyCodeKata
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public static int[] MonkeyCount(int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = i + 1;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+
+            Console.WriteLine();
+
+        }
     }
-  }
 }
